f91a3af31386c629e0c0095066d45468230fecf5
wzl <1920731406@qq.com>
2022-06-10 19:39:04 +0800

    suanti

---
+++ src/main/java/stack.java
@@ -0,0 +1,47 @@
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+/**
+ * 设计一个特殊的栈，在实现栈的基本功能的基础上，再实现返回栈中最小元素的操作。
+ *
+ * 1. pop. push、getMin操作的时间复杂度都是O(1)。
+ * 2．设计的栈类型可以使用现成的栈结构。
+ */
+
+@Data
+public class stack {
+
+    private Stack<Integer>  stackData;
+    private  Integer  stackMin;
+
+    public stack(){
+        this.stackData=new Stack<Integer>();
+    }
+
+    public void  push(Integer num){
+        if(stackData.empty()){
+            stackData.push(num);
+            this.stackMin=num;
+        }else{
+            if(num<stackMin){
+                stackMin=num;
+            }
+            stackData.push(num);
+        }
+    }
+
+    public Integer pop(){
+        return stackData.pop();
+    }
+
+    public Integer getMin(){
+        if (stackData.empty()){
+            throw new RuntimeException("栈为空");
+        }
+        return this.stackMin;
+    }
+
+
+}
---
+++ src/main/java/stackTest.java
@@ -0,0 +1,13 @@
+public class stackTest {
+
+    private static stack stack;
+
+    public static void main(String[] args) {
+
+                   stack = new stack();
+                   for (int  i=10;i<100;i++){
+                       stack.push((Integer) i);
+                   }
+                   System.out.println(stack.getMin());
+    }
+}
