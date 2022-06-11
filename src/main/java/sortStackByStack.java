5d2a5d13abd2d4ed4008ead928f7ba72b98720af
wzl <1920731406@qq.com>
2022-06-11 20:33:04 +0800

    suanti

--- src/main/java/sortStackByStack.java
+++ src/main/java/sortStackByStack.java
@@ -1,2 +1,48 @@
+import java.util.Stack;
+
 public class sortStackByStack {
+
+    public Stack<Integer> stack;
+    public Stack<Integer>  help;
+
+    public sortStackByStack(){
+        this.stack=new Stack<>();
+        this.help=new Stack<>();
+    }
+
+    public void sort(){
+
+        while(!stack.empty()){
+
+            Boolean k =false;
+            int temp=stack.pop();
+
+            if(help.empty()){
+                help.push(temp);
+            }else if(temp<=help.peek()){
+                help.push(temp);
+            }else{
+                while(temp>help.peek()){
+                    int temp2=help.pop();
+                    if(help.empty()){
+                        help.push(temp);
+                        k=true;
+                    }
+                    stack.push(temp2);
+                }
+
+                if(k==true){
+                    help.push(temp);
+                }
+
+            }
+        }
+
+        while(!help.empty()){
+            stack.push(help.pop());
+        }
+
+    }
+
+
 }
--- src/main/java/sortStackByStackTest.java
+++ src/main/java/sortStackByStackTest.java
@@ -1,2 +1,20 @@
+import java.util.Stack;
+
 public class sortStackByStackTest {
+
+    public static void main(String[] args) {
+        sortStackByStack  stack=new sortStackByStack();
+
+        for (int i =100;i>0;i--){
+            stack.stack.push(i);
+        }
+
+        System.out.println(stack.stack);
+
+        stack.sort();
+
+        System.out.println(stack.stack);
+
+
+    }
 }
